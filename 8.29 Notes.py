9e04485f85a52bd8538035c7dd0aaecc2b60c500
ninjahawk <40222252+ninjahawk@users.noreply.github.com>
2024-08-29 15:18:32 -0400

    Add files via upload

---
+++ 8.29 Notes.py
@@ -0,0 +1,31 @@
+8/29 Notes
+
+record1 = "John Doe 28 2382 ElmStreet Durham 28312"
+
+Two types of numbers:
+
+    int = (1,2,3)
+
+    float = decimal numbers
+
+Variables:
+
+    x = 5
+
+    print(x + 3) = 8
+
+    print(x) = 5
+
+Multiplication = *
+
+Exponent = **
+
+Data Types:
+
+    String
+    Int
+    Float
+
+
+
+    
